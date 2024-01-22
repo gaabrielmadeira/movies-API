6876077b53fb3973d7ea1251dd071eb59c865310
Gabriel Madeira <gabscastello95@gmailcom>
2024-01-22 16:51:13 -0300

    doc

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,14 @@ import "express-async-errors";
 import express, { Application } from "express";
 import { moviesRouter } from "./routers";
 import { handleErrors } from "./middlewares/handleErrors.middleware";
+import swaggerUI from "swagger-ui-express";
+import swaggerDocument from "../swagger.json";
 
 const app: Application = express();
 app.use(express.json());
 
 app.use("/movies", moviesRouter);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 app.use(handleErrors);
 
--- src/middlewares/pagination.middleware.ts
+++ src/middlewares/pagination.middleware.ts
@@ -8,7 +8,7 @@ const pagination = async (req: Request, res: Response, next: NextFunction): Prom
   const page: number = Number.isInteger(queryPage) && queryPage > 0 ? queryPage : 1;
   const perPage: number = Number.isInteger(queryPerPage) && queryPerPage > 0 && queryPerPage <= 5 ? queryPerPage : 5;
 
-  const baseUrl: string = "http://db:3000/movies";
+  const baseUrl: string = "http://localhost:3000/movies";
   const prevPage: string = `${baseUrl}?page=${page - 1}&perPage=${perPage}`;
   const nextPage: string = `${baseUrl}?page=${page + 1}&perPage=${perPage}`;
 
